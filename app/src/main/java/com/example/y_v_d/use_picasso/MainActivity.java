6cf3746a2e9502f936f613f5e075df77b6eeae77
caemooshin <mooshin4@gmail.com>
2017-05-11 15:28:42 +0700

    add image view2

--- app/src/main/java/com/example/y_v_d/use_picasso/MainActivity.java
+++ app/src/main/java/com/example/y_v_d/use_picasso/MainActivity.java
@@ -6,20 +6,24 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    //@BindView(R.id.ivBasicImage)
-     ImageView ivBasicImage;
+    @BindView(R.id.imageView2) ImageView imageView2;
+    @BindView(R.id.imageView) ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //ButterKnife.bind(this);
+        ButterKnife.bind(this);
         String imageUri = "https://i.imgur.com/tGbaZCY.jpg";
-        ivBasicImage = (ImageView) findViewById(R.id.ivBasicImage);
-        Picasso.with(getApplicationContext()).load(imageUri).into(ivBasicImage);
+        //ivBasicImage = (ImageView) findViewById(R.id.ivBasicImage);
+        Picasso.with(getApplicationContext()).load(imageUri).into(imageView2);
+        Picasso.with(getApplicationContext()).load(imageUri).into(imageView);
 
     }
 }
